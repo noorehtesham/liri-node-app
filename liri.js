498e1f334ead2cc6e4713504a642443b2885c049
Noor Ehtesham <noore44@vt.edu>
2018-03-26 20:22:05 -0400

    changes

--- liri.js
+++ liri.js
@@ -54,18 +54,6 @@ function spotify(songName) {
         id: "9fd36a0e5ab64d36adb0bf79527dacc9",
         secret: "a710149eb69f4990bd6e276f54f9c2e1"
     });
-    spotifyApi.getArtists(['2hazSY4Ef3aB9ATXW7F5w3', '6J6yx1t3nwIDyPXk5xa7O8'])
-        .then(function (data) {
-            console.log('Artists information', data.body);
-        }, function (err) {
-            console.error(err);
-        });
-    spotifyApi.getAlbums(['5U4W9E5WsYb2jUQWePT8Xm', '3KyVcddATClQKIdtaap4bV'])
-        .then(function (data) {
-            console.log('Albums information', data.body);
-        }, function (err) {
-            console.error(err);
-        });
 
     spotify.search({
         type: 'track',
@@ -81,9 +69,10 @@ function spotify(songName) {
 
 function movieThis(movieTitle) {
 
-    var movieName = "";
-    var movieTitle = process.argv[3] || "Mr. Nobody";
+    var movieName= "";
+    movieTitle = movieTitle || process.argv[3] || "Mr. Nobody";
 
+// encode the URL with %20
     for (var i = 2; i < movieTitle.length; i++) {
         if (i > 2 && i < movieTitle.length) {
             movieName = movieName + "+" + movieTitle[i];
@@ -105,9 +94,6 @@ function movieThis(movieTitle) {
           
 
 
-            // Saw (2004) 7.6/10 
-            // Two men wake up at opposite sides of a dirty, disused bathroom, chained 
-            // by their ankles to pipes. Between them lies... 
         };
 
     })
@@ -120,11 +106,26 @@ function doWhatitSays() {
         if (error) {
             return console.log(error);
         }
-        console.log(data);
+       
 
         var dataArr = data.split(",");
-
-        console.log(dataArr);
+        
+//        
+        switch(dataArr[0]) {
+            case "my-tweets":
+                myTweets();
+                break;
+            case "spotify-this-song":
+                spotify(dataArr[1]);
+                break;
+            case "movie-this":
+                movieThis(dataArr[1]);
+                
+                break;
+                
+        }
+       
+       
     });
 
 }
